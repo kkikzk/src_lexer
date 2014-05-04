9ab60fc95abc38f11f2634a654a5ecdd09865ec4
kkikzk <kkikzk@gmail.com>
2014-05-04 12:11:43 +0900

    add test case

--- spec/src_lexer_spec.rb
+++ spec/src_lexer_spec.rb
@@ -3,7 +3,7 @@ require_relative './spec_helper'
 
 describe SrcLexer do
   it 'should have a version number' do
-    SrcLexer::VERSION.should_not be_nil
+    expect(SrcLexer::VERSION).not_to eq(be_nil)
   end
 end
 
@@ -11,7 +11,7 @@ describe SrcLexer::Lexer, 'with empty string' do
   it 'should return Lexer::END_TOKEN' do
     sut = SrcLexer::Lexer.new(nil, nil, nil, nil, nil)
     sut.analyze('')
-    sut.pop_token.should == SrcLexer::Lexer::END_TOKEN
+    expect(sut.pop_token).to eq(SrcLexer::Lexer::END_TOKEN)
   end
 end
 
@@ -19,18 +19,18 @@ describe SrcLexer::Lexer, 'with keyword definitions' do
   it 'should recognize keywords' do
     sut = SrcLexer::Lexer.new(['struct', 'enum'], nil, nil, nil, nil)
     sut.analyze('struct structenum enum')
-    sut.pop_token.should == ['struct', SrcLexer::Token.new('struct', 1, 1)]
-    sut.pop_token.should == [:IDENT, SrcLexer::Token.new('structenum', 1, 8)]
-    sut.pop_token.should == ['enum', SrcLexer::Token.new('enum', 1, 19)]
-    sut.pop_token.should == SrcLexer::Lexer::END_TOKEN
+    expect(sut.pop_token).to eq(['struct', SrcLexer::Token.new('struct', 1, 1)])
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('structenum', 1, 8)])
+    expect(sut.pop_token).to eq(['enum', SrcLexer::Token.new('enum', 1, 19)])
+    expect(sut.pop_token).to eq(SrcLexer::Lexer::END_TOKEN)
   end
   it 'should reduce keyword duplication' do
     sut = SrcLexer::Lexer.new(['struct', 'struct'], nil, nil, nil, nil)
-    sut.keywords.should == ['struct']
+    expect(sut.keywords).to eq(['struct'])
   end
   it 'should ignore nil keyword' do
     sut = SrcLexer::Lexer.new(['struct', nil, 'enum'], nil, nil, nil, nil)
-    sut.keywords.should == ['struct', 'enum']
+    expect(sut.keywords).to eq(['struct', 'enum'])
   end
 end
 
@@ -38,26 +38,36 @@ describe SrcLexer::Lexer, 'with symbol definitions' do
   it 'should recognize symbols' do
     sut = SrcLexer::Lexer.new(nil, ['..', ','], nil, nil, nil)
     sut.analyze('.. A ,')
-    sut.pop_token.should == ['..', SrcLexer::Token.new('..', 1, 1)]
-    sut.pop_token.should == [:IDENT, SrcLexer::Token.new('A', 1, 4)]
-    sut.pop_token.should == [',', SrcLexer::Token.new(',', 1, 6)]
-    sut.pop_token.should == SrcLexer::Lexer::END_TOKEN
+    expect(sut.pop_token).to eq(['..', SrcLexer::Token.new('..', 1, 1)])
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('A', 1, 4)])
+    expect(sut.pop_token).to eq([',', SrcLexer::Token.new(',', 1, 6)])
+    expect(sut.pop_token).to eq(SrcLexer::Lexer::END_TOKEN)
   end
   it 'should recognize symbols(,) if continues like "A,B"' do
     sut = SrcLexer::Lexer.new(['A', 'B'], [','], nil, nil, nil)
     sut.analyze('A,B')
-    sut.pop_token.should == ['A', SrcLexer::Token.new('A', 1, 1)]
-    sut.pop_token.should == [',', SrcLexer::Token.new(',', 1, 2)]
-    sut.pop_token.should == ['B', SrcLexer::Token.new('B', 1, 3)]
-    sut.pop_token.should == SrcLexer::Lexer::END_TOKEN
+    expect(sut.pop_token).to eq(['A', SrcLexer::Token.new('A', 1, 1)])
+    expect(sut.pop_token).to eq([',', SrcLexer::Token.new(',', 1, 2)])
+    expect(sut.pop_token).to eq(['B', SrcLexer::Token.new('B', 1, 3)])
+    expect(sut.pop_token).to eq(SrcLexer::Lexer::END_TOKEN)
+  end
+  it 'should recognize symbol(==) if symbol(=) defined' do
+    sut = SrcLexer::Lexer.new(nil, ['==', '='], nil, nil, nil)
+    sut.analyze('A = B == C')
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('A', 1, 1)])
+    expect(sut.pop_token).to eq(['=', SrcLexer::Token.new('=', 1, 3)])
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('B', 1, 5)])
+    expect(sut.pop_token).to eq(['==', SrcLexer::Token.new('==', 1, 7)])
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('C', 1, 10)])
+    expect(sut.pop_token).to eq(SrcLexer::Lexer::END_TOKEN)
   end
   it 'should reduce symbol duplication' do
     sut = SrcLexer::Lexer.new(nil, [',', ','], nil, nil, nil)
-    sut.symbols.should == [',']
+    expect(sut.symbols).to eq([','])
   end
   it 'should ignore nil keyword' do
     sut = SrcLexer::Lexer.new(nil, ['{', nil, '}'], nil, nil, nil)
-    sut.symbols.should == ['{', '}']
+    expect(sut.symbols).to eq(['{', '}'])
   end
 end
 
@@ -68,9 +78,9 @@ describe SrcLexer::Lexer, 'with line comment marker' do
       A//comment
       B
     EOS
-    sut.pop_token.should == [:IDENT, SrcLexer::Token.new('A', 1, 7)]
-    sut.pop_token.should == [:IDENT, SrcLexer::Token.new('B', 2, 7)]
-    sut.pop_token.should == SrcLexer::Lexer::END_TOKEN
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('A', 1, 7)])
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('B', 2, 7)])
+    expect(sut.pop_token).to eq(SrcLexer::Lexer::END_TOKEN)
   end
   it 'should recognize multi line comment' do
     sut = SrcLexer::Lexer.new(nil, nil, nil, '//', ['/*', '*/'])
@@ -78,9 +88,9 @@ describe SrcLexer::Lexer, 'with line comment marker' do
       A/*comment
       B//still in comment*/C
     EOS
-    sut.pop_token.should == [:IDENT, SrcLexer::Token.new('A', 1, 7)]
-    sut.pop_token.should == [:IDENT, SrcLexer::Token.new('C', 2, 28)]
-    sut.pop_token.should == SrcLexer::Lexer::END_TOKEN
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('A', 1, 7)])
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('C', 2, 28)])
+    expect(sut.pop_token).to eq(SrcLexer::Lexer::END_TOKEN)
   end
 end
 
@@ -88,18 +98,18 @@ describe SrcLexer::Lexer do
   it 'should analyze number string' do
     sut = SrcLexer::Lexer.new(nil, nil, nil, nil, nil)
     sut.analyze('9 1.5')
-    sut.pop_token.should == [:NUMBER, SrcLexer::Token.new("9", 1, 1,)]
-    sut.pop_token.should == [:NUMBER, SrcLexer::Token.new("1.5", 1, 3)]
-    sut.pop_token.should == SrcLexer::Lexer::END_TOKEN
+    expect(sut.pop_token).to eq([:NUMBER, SrcLexer::Token.new("9", 1, 1,)])
+    expect(sut.pop_token).to eq([:NUMBER, SrcLexer::Token.new("1.5", 1, 3)])
+    expect(sut.pop_token).to eq(SrcLexer::Lexer::END_TOKEN)
   end
   it 'should analyze string literal' do
     sut = SrcLexer::Lexer.new(nil, nil, ['"', '"'], '//', ['/*', '*/'])
     sut.analyze('A"//"B"/**/"C')
-    sut.pop_token.should == [:IDENT, SrcLexer::Token.new('A', 1, 1)]
-    sut.pop_token.should == [:STRING, SrcLexer::Token.new('"//"', 1, 2)]
-    sut.pop_token.should == [:IDENT, SrcLexer::Token.new('B', 1, 6)]
-    sut.pop_token.should == [:STRING, SrcLexer::Token.new('"/**/"', 1, 7)]
-    sut.pop_token.should == [:IDENT, SrcLexer::Token.new('C', 1, 13)]
-    sut.pop_token.should == SrcLexer::Lexer::END_TOKEN
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('A', 1, 1)])
+    expect(sut.pop_token).to eq([:STRING, SrcLexer::Token.new('"//"', 1, 2)])
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('B', 1, 6)])
+    expect(sut.pop_token).to eq([:STRING, SrcLexer::Token.new('"/**/"', 1, 7)])
+    expect(sut.pop_token).to eq([:IDENT, SrcLexer::Token.new('C', 1, 13)])
+    expect(sut.pop_token).to eq(SrcLexer::Lexer::END_TOKEN)
   end
 end
